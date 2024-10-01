7bc0bbc2cb017c9cdc3da326239adc6e9ab79f27
Raul Albín <raul200794@gmail.com>
2024-10-01 22:43:14 +0200

    Initial Commit

---
+++ playaround/playaroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  playaroundApp.swift
+//  playaround
+//
+//  Created by Raul Albin on 1/10/24.
+//
+
+import SwiftUI
+
+@main
+struct playaroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ playaroundTests/playaroundTests.swift
@@ -0,0 +1,17 @@
+//
+//  playaroundTests.swift
+//  playaroundTests
+//
+//  Created by Raul Albin on 1/10/24.
+//
+
+import Testing
+@testable import playaround
+
+struct playaroundTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
